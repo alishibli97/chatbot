21b198972240bfe288984872a5233f434df176b0
Ali Shibli <30579166+alishibli97@users.noreply.github.com>
2020-01-09 05:51:42 +0200

    Added option to read from files

--- classification/classifier_regex.py
+++ classification/classifier_regex.py
@@ -1,6 +1,75 @@
 import json
 import re
+import pandas as pd
 
+def check_categories(sentence):
+    cat_found=[]
+    for category in labels:    
+        for phrase in labels[category][1]:
+            x=re.search("(^|[^a-zA-Z])"+phrase+"($|[^a-zA-Z])",sentence,re.IGNORECASE)
+            if(x is not None): 
+                cat_found.append(category)
+                break
+    return cat_found
+    
+def check_hierarchy(cat_found):
+    if(cat_found==[]): return "Please be more precise",-1
+    elif(cat_found==["Greetings"]): return "Greetings",0
+    elif (len(cat_found)>=1):
+        if("Greetings" in cat_found): cat_found.remove("Greetings")
+        if(len(cat_found)==1): return cat_found[0],labels[cat_found[0]][0]
+        elif("Error" in cat_found): return "Error",2
+        elif("Syntax" in cat_found): return "Syntax",3
+        elif("Interpreted" in cat_found): return "Interpreted",4
+        elif("Directory" in cat_found): return "Directory",6
+        elif("Methods" in cat_found): return "Method",5
+        else: return 1
+
+def input_user():
+    print("\nPlease enter exit to exit.")
+    while True:
+        sentence=input("\nPlease input your sentence: ")
+        if(sentence!="exit"):
+            cat_found = check_categories(sentence)
+            print("Categories found: ",cat_found)
+            name,num=check_hierarchy(cat_found)
+            if num==-1: print("Please be more precise.")
+            else: print("Belongs to {}, category {}".format(name,num))
+        else: break
+
+def input_file():
+    df = pd.read_csv('added_questions.csv', encoding='cp1252')
+    col = ['Type', 'user1']
+    df = df[col]
+    
+    TP=0 # false positives
+    TN=0 # true positives
+    FP=0 # false positives
+    FN=0 # false negatives
+    
+    for question in df['user1']:
+        cat = df.loc[df['user1'] == question]
+        cat = int(cat['Type'])
+        
+        cat_found=check_categories(question)
+        name,num=check_hierarchy(cat_found)
+        
+        if num==-1:
+            if num==cat: TN+=1
+            else: FN+=1
+        elif num!=-1:
+            if num==cat: TP+=1
+            else: FP+=1
+    
+    confusion_matrix = pd.DataFrame([[TN,FP],[FN,TP]])
+    
+    accuracy = (TP+TN)/(TP+TN+FP+FN)
+    recall = TP/(TP+FN)
+    precision = TP/(TP+FP)
+    
+    print("Confusion matrix:\n",confusion_matrix)
+    print("Accuary: {}\nRecall: {}\nPrecision: {}".format(accuracy,recall,precision))
+    
 with open("annotations.json") as json_file:
     data = json.load(json_file)
     
@@ -51,31 +120,6 @@ with open("annotations.json") as json_file:
             for value in x:
                 if x not in labels[category][1]: labels[category][1].append(value)
             file.close()
-    
-    print("\nPlease enter exit to exit.")
-    while True:
-        sentence=input("\nPlease input your sentence: ")
-        if(sentence!="exit"):
-            cat_found=[]
-            for category in labels:    
-                for phrase in labels[category][1]:
-                    #x=re.search("(^|\s)"+phrase+"($|\s)",sentence,re.IGNORECASE)
-                    x=re.search("(^|[^a-zA-Z])"+phrase+"($|[^a-zA-Z])",sentence,re.IGNORECASE)
-                    if(x is not None): 
-                        cat_found.append(category)
-                        break
-            print("Categories found: ",cat_found)
-            if(cat_found==[]): print("Please be more precise.")
-            elif(cat_found==["Greetings"]): print("Belongs to Greetings, category 0.")
-            elif (len(cat_found)>=1):
-                    if("Greetings" in cat_found): cat_found.remove("Greetings")
-                    if(len(cat_found)==1): print("Belongs to {}, category {}.".format(cat_found[0],labels[cat_found[0]][0]))
-                    elif("Error" in cat_found): print("Belongs to Error, category 2.")
-                    elif("Syntax" in cat_found): print("Belongs to Syntax, category 3.")
-                    elif("Interpreted" in cat_found): print("Belongs to Interpreted, category 4.")
-                    elif("Directory" in cat_found): print("Belongs to Directory, category 6.")
-                    elif("Methods" in cat_found): print("Belongs to Methods, category 5.")
-                    else: print("Belongs to Library, category 1.")
-        else: break
-        
-        
+            
+    #input_user()
+    input_file()
